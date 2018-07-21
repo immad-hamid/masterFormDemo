056b76ac5939c7fe261e5f6445fe221574fb9f08
Immad Hamid <immad.hamid@gmail.com>
2018-07-21 17:01:39 +0500

    - Add, Update and Delete working in table rows.

---
+++ src/app/models/customers.model.ts
@@ -0,0 +1,4 @@
+export interface Icustomers {
+    name: string,
+    age: number
+}
--- src/app/views/master-form/detail/detail.component.ts
+++ src/app/views/master-form/detail/detail.component.ts
@@ -7,12 +7,11 @@ import { SubjectService } from '../../../services/subject.service';
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
+
 export class DetailComponent implements OnInit {
   masterFormData: any;
-  detailGridForm: any;
   activities: any;
   resources: any;
-  formEl: FormGroup;
 
   private fieldArray: Array<any> = [];
   private newAttribute: any = {};
@@ -22,12 +21,10 @@ export class DetailComponent implements OnInit {
   ) {
     this.fetchActivities((data) => {
       this.activities = [...data];
-      console.log(this.activities);
     });
 
     this.fetchResources((data) => {
       this.resources = [...data];
-      console.log(this.resources);
     });
 
     this.subService.headerData.subscribe((data) => {
@@ -37,13 +34,29 @@ export class DetailComponent implements OnInit {
   }
 
   addFieldValue() {
+    this.newAttribute.disabled = true;
+    this.newAttribute.rowEditMode = false;
     this.fieldArray.push(this.newAttribute)
     this.newAttribute = {};
     console.log(this.fieldArray);
   }
 
+  editFieldValue(index) {
+    this.fieldArray[index].disabled = false;
+    this.fieldArray[index].rowEditMode = true;
+  }
+
+  addAfterEdit(index) {
+    this.fieldArray[index].disabled = true;
+    this.fieldArray[index].rowEditMode = false;
+  }
+
   deleteFieldValue(index) {
     this.fieldArray.splice(index, 1);
+  }
+
+  submit(f) {
+    console.log(f);
   }
 
   ngOnInit() { }
@@ -72,51 +85,51 @@ export class DetailComponent implements OnInit {
     req.send();
   }
 
-  saveData() {
-    const obj = {
-      status: '2',
-      ACTIVITY: '',
-      RESOURCE: '',
-      OPRN_ID: '0',
-      OPRN_NO: '',
-      OPRN_VERS: '2',
-      OPRN_NAME: this.masterFormData.operation,
-      OPERATION_STATUS: this.masterFormData.status,
-      OPRN_CLASS_ID: this.masterFormData.class,
-      OPRN_CLASS: '',
-      OPRN_CLASS_NAME: '',
-      ORGANIZATION_ID: '1947',
-      MFG_OPERATION_DETAILS: [{
-        OPRN_ACT_RES_ID: 0,
-        OPRN_ACT_ID: 190,
-        RESOURCE_ID: this.resourcesf.value,
-        LINE_NO: this.lineNum.value,
-        OPRN_ID: 1781,
-        ACTIVITY_ID: parseInt(this.activity.value),
-        ACTIVITY: 1,
-        ACTIVITY_NAME: 2,
-        RESOURCE: 158,
-        RESOURCE_NAME: null,
-        RESOURCE_USAGE: '2017-07-01T00:00:00',
-        USAGE_UM: null,
-        PROCESS_QTY: this.proQuan.value,
-        PROCESS_UOM: ''
-      }]
-    }
+  // saveData() {
+  //   const obj = {
+  //     status: '2',
+  //     ACTIVITY: '',
+  //     RESOURCE: '',
+  //     OPRN_ID: '0',
+  //     OPRN_NO: '',
+  //     OPRN_VERS: '2',
+  //     OPRN_NAME: this.masterFormData.operation,
+  //     OPERATION_STATUS: this.masterFormData.status,
+  //     OPRN_CLASS_ID: this.masterFormData.class,
+  //     OPRN_CLASS: '',
+  //     OPRN_CLASS_NAME: '',
+  //     ORGANIZATION_ID: '1947',
+  //     MFG_OPERATION_DETAILS: [{
+  //       OPRN_ACT_RES_ID: 0,
+  //       OPRN_ACT_ID: 190,
+  //       RESOURCE_ID: this.resourcesf.value,
+  //       LINE_NO: this.lineNum.value,
+  //       OPRN_ID: 1781,
+  //       ACTIVITY_ID: parseInt(this.activity.value),
+  //       ACTIVITY: 1,
+  //       ACTIVITY_NAME: 2,
+  //       RESOURCE: 158,
+  //       RESOURCE_NAME: null,
+  //       RESOURCE_USAGE: '2017-07-01T00:00:00',
+  //       USAGE_UM: null,
+  //       PROCESS_QTY: this.proQuan.value,
+  //       PROCESS_UOM: ''
+  //     }]
+  //   }
 
-    const req = new XMLHttpRequest();
-    req.open('POST', `http://C3-0467:8011/api/Values/Save`, true);
-    req.setRequestHeader('Content-type', 'application/json');
+  //   const req = new XMLHttpRequest();
+  //   req.open('POST', `http://C3-0467:8011/api/Values/Save`, true);
+  //   req.setRequestHeader('Content-type', 'application/json');
 
-    req.onreadystatechange = function () {//Call a function when the state changes.
-      if (req.readyState == 4 && req.status == 200) {
-        alert(JSON.parse(req.responseText));
-        console.log(JSON.stringify(obj));
-        // alert(req.responseText);
-      }
-    }
+  //   req.onreadystatechange = function () {//Call a function when the state changes.
+  //     if (req.readyState == 4 && req.status == 200) {
+  //       alert(JSON.parse(req.responseText));
+  //       console.log(JSON.stringify(obj));
+  //       // alert(req.responseText);
+  //     }
+  //   }
 
-    req.send(JSON.stringify(obj));
-  }
+  //   req.send(JSON.stringify(obj));
+  // }
 
 }
